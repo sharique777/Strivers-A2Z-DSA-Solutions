540e67b1e7e73bf99fb2fedd70353460e8fb6c80
sharique777 <sharique.keyam2@gmail.com>
2026-08-01 13:52:47 +0530

    Version 1.1: implemented full traingle

--- Pattern 7.js
+++ Pattern 7.js
@@ -1,13 +1,25 @@
-n=4
+//Took 31 mins
+
+ n=2
+c=0
 str=""
 for (let i = 0 ; i < n; i++){
   for (let j = 0 ; j < n; j++){
     if ( n-j<=i+1){
 str+="*"
+c++
+if (c  === i+1){
+
+      for (let k = 0 ; k < i-1+1; k++){
+str+="*"
+//console.log(k); 
+} 
+} 
 } else {
   str+=" "
 } 
 }
 console.log(str); 
 str=""
+c=0
 }
