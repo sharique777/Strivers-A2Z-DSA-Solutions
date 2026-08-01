7029724bb54a982810e2bfec90b80d7ed75c9315
sharique777 <sharique.keyam2@gmail.com>
2026-08-01 12:53:48 +0530

    Version 1.3: implemented 1st and second quadrant

--- pattern22.js
+++ pattern22.js
@@ -4,48 +4,40 @@ let rightHalfPreviousLimit = 0;
 let leftHalfLastValue = 0;
 let leftHalfPreviousLimit = 0;
 
-let size = 5;
-
+let size = 2;
+let result=[]
 for (let row = 0; row < size; row++) {
     let descendingPart = "";
-let ascendingPart = "";
+    let ascendingPart = "";
     for (let currentValue = size; currentValue > 0; currentValue--) {
         if (currentValue <= rightHalfPreviousLimit) {
-            descendingPart += (rightHalfPreviousLimit + 1) + " ";
-            if (currentValue !==1 ){
-
-ascendingPart = (rightHalfPreviousLimit + 1) + " "+ascendingPart;
-} 
+            descendingPart += rightHalfPreviousLimit + 1 + " ";
+            if (currentValue !== 1) {
+                ascendingPart =
+                    rightHalfPreviousLimit + 1 + " " + ascendingPart;
+            }
             rightHalfLastValue = currentValue + rightHalfPreviousLimit;
-            
         } else {
             descendingPart += currentValue + " ";
-            if ( currentValue!==1 ){
-
-ascendingPart = currentValue + " "+ascendingPart;
-} 
+            if (currentValue !== 1) {
+                ascendingPart = currentValue + " " + ascendingPart;
+            }
             rightHalfLastValue = currentValue;
         }
     }
 
     rightHalfPreviousLimit = rightHalfLastValue;
 
+
+    //console.log(descendingPart.trim() + " " + ascendingPart.trim());
+    result.push(descendingPart.trim() + " " + ascendingPart.trim())
     
-
-    /* for (let currentValue = 2; currentValue <= size; currentValue++) {
-        if (size - currentValue <= leftHalfPreviousLimit) {
-            ascendingPart += (leftHalfPreviousLimit + 1) + " ";
-            leftHalfLastValue = currentValue + leftHalfPreviousLimit;
-        } else {
-            ascendingPart += currentValue + " ";
-
-            if (currentValue === 2) {
-                leftHalfLastValue = currentValue;
-            }
-        }
-    } */
-
-    console.log(descendingPart.trim() + " " +ascendingPart.trim());
 
     leftHalfPreviousLimit = leftHalfLastValue;
 }
+for (let i =  result. length -1; i >= 0; i--){
+      console.log(result[i]); 
+} 
+for (let i = 1 ; i < result. length ; i++){
+      console.log(result[i]); 
+} 
