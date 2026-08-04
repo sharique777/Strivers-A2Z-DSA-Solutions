16296a792fac42b5999c98545425f23d4c20514d
sharique777 <sharique.keyam2@gmail.com>
2026-08-04 22:00:56 +0530

    Feat: add butterfly star pattern generator

---
+++ Pattern 19.js
@@ -0,0 +1,28 @@
+let n=5
+let str=""
+for (let i = n ; i >=1; i--){
+for (let j = 1 ; j <=i; j++){
+  str+="*"
+} 
+for (let j = 1 ; j <=2*(n-i); j++){
+  str+=" "
+} 
+for (let j = 1 ; j <=i; j++){
+  str+="*"
+} 
+console.log(str); 
+str=""
+}  
+for (let i = 1 ; i <=n; i++){
+for (let j = 1 ; j <=i; j++){
+  str+="*"
+} 
+for (let j = 1 ; j <=2*(n-i); j++){
+  str+=" "
+} 
+for (let j = 1 ; j <=i; j++){
+  str+="*"
+} 
+console.log(str); 
+str=""
+}  
