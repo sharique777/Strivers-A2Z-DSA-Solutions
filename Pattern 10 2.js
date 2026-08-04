5187938c42fac3730abb48f8795a19072b53769d
sharique777 <sharique.keyam2@gmail.com>
2026-08-04 20:08:20 +0530

    feat: add pattern printing logic for star pyramid

---
+++ Pattern 10 2.js
@@ -0,0 +1,16 @@
+let n = 5;
+let str = "";
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        str += "*";
+    }
+    console.log(str);
+    str = "";
+}
+for (let i = n - 1; i >= 1; i--) {
+    for (let j = 1; j <= i; j++) {
+        str += "*";
+    }
+    console.log(str);
+    str = "";
+}
