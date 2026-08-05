939be35cb93c55e94bb5a13bf9410c7710d2176f
sharique777 <sharique.keyam2@gmail.com>
2026-08-05 18:21:24 +0530

    refactor: use object instead of array for frequency counting

--- Counting Frequencies of Array Elements.js
+++ Counting Frequencies of Array Elements.js
@@ -1,14 +1,11 @@
 let arr = [1, 3, 4, 1];
 let output_arr = [];
-let frequ = [];
+let frequ = {};
 for (const value of arr) {
     frequ[value] = (frequ[value] || 0) + 1;
 }
 for (const index in frequ ) {
 
-if ( frequ[index]== undefined){
-continue
-} 
     output_arr.push([Number(index), frequ[index]]);
 }
 console.log(output_arr);
