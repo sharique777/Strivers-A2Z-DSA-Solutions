0bd138a6990581c355afff863cc3785b0d2a6dd1
sharique777 <sharique.keyam2@gmail.com>
2026-08-05 18:09:27 +0530

    fix: correct frequency counting logic and add output array mapping

--- Counting Frequencies of Array Elements.js
+++ Counting Frequencies of Array Elements.js
@@ -1,5 +1,10 @@
 let arr = [1, 3, 4, 1];
+let output_arr = [];
 let frequ = [];
-for (const iterator_value of arr) {
-    frequ[value] = frequ[value] || 0 + 1;
+for (const value of arr) {
+    frequ[value] = (frequ[value] || 0) + 1;
 }
+for (const [index, value] of arr.entries()) {
+    output_arr.push([value, frequ[value]]);
+}
+console.log(output_arr);
