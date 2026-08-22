b4c0f672b850eafe4002cfbd711187b2ead53f24
sharique777 <sharique.keyam2@gmail.com>
2026-08-22 18:20:50 +0530

    feat:: imp lement binary search function

---
+++ Search X in sorted array.js
@@ -0,0 +1,18 @@
+function binarySearch(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+}
