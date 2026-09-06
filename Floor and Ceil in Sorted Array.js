51760b493776542d1867520761106bd7befdfcda
sharique777 <sharique.keyam2@gmail.com>
2026-09-06 14:12:56 +0530

    feat: implement getFloorCeil function to find floor and ceil values

---
+++ Floor and Ceil in Sorted Array.js
@@ -0,0 +1,28 @@
+function getFloorCeil(numbers, target) {
+  let low = 0;
+  let high = numbers.length - 1;
+  let floorValue = -1;
+  let ceilValue = -1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (numbers[mid] === target) {
+      floorValue = numbers[mid];
+      ceilValue = numbers[mid];
+      break;
+    } else if (numbers[mid] < target) {
+      floorValue = numbers[mid];
+      low = mid + 1;
+    } else {
+      ceilValue = numbers[mid];
+      high = mid - 1;
+    }
+  }
+
+  return (floorValue??-1) + " " + (ceilValue??-1);
+}
+
+const numbers = [2, 4, 6, 8];
+const targetValue = 7;
+console.log(getFloorCeil(numbers, targetValue));
